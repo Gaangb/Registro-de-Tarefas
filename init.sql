56d0c0067bb58ec10b73976d072d72bd61902091
Gaangb <angelob4stos@gmail.com>
2024-01-22 18:58:27 -0300

    feat: adicionando arquivo para criacao de tabela tasks

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Tasks (
+    id SERIAL PRIMARY KEY,
+    titulo_tarefa VARCHAR(100) NOT NULL,
+    descricao_tarefa VARCHAR(255),
+    data_tarefa DATE NOT NULL,
+    status_tarefa VARCHAR(20) NOT NULL,
+    CHECK (status_tarefa IN ('executando', 'concluída', 'pendente'))
+);
+
