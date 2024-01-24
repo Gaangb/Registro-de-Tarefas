3d6756bf0a57d82de122b2182b7148615336b9ab
Gaangb <angelob4stos@gmail.com>
2024-01-24 11:01:08 -0300

    feat: adicionando a logica na pagina home para funcionamento do form

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useCustomer } from "../../hooks/CustomerHooks";
 import Container from "../../components/Container";
 import TasksList from "../../components/TasksList";
 import Header from "../../components/Header";
 
 export default function Home() {
+  const {
+    showformTask,
+  } = useCustomer();
   return (
-    <Container customClass="customer_container container">
+    <Container customClass={`customer_container container ${showformTask ? "modal-open" : ""}`}>
       <Header />
       <TasksList />
     </Container>
