7d7f75c8404dc0d193fb7900d7fc0ee02fe002ba
Gaangb <angelob4stos@gmail.com>
2024-01-24 22:17:37 -0300

    clean code

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,3 @@
-# # app/__init__.py
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# # Carregue a configuração da instância do Flask a partir do arquivo de configuração
-# # app.config.from_object('config')
-
-# # Importe os módulos de rotas
-# from app.routes import task_routes
-
-# app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -21,9 +9,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    # Configuração da aplicação, se necessário
 
-    # Registre o Blueprint
     from .routes import main
     app.register_blueprint(main)
 
