91d9fb25ea1e324fab766ee87137343592eda405
Gaangb <angelob4stos@gmail.com>
2024-01-28 01:06:01 -0300

    feat: adicionando deletetask e gettask, e custom class

--- frontend/src/components/TaskCard/index.jsx
+++ frontend/src/components/TaskCard/index.jsx
@@ -1,60 +1,56 @@
+import { memo, useEffect, useState } from 'react';
 import { VscEdit, VscTrash } from "react-icons/vsc";
-import { useCustomer } from "../../hooks/CustomerHooks";
 import "./style.css";
 
-export default function TaskCard({
-  id,
-  title,
-  taskDescription,
-  taskDate,
-  taskStatus,
+function TaskCard({
+  getTaskById,
+  deleteTaskById,
+  note,
+  setShowFormTask,
 }) {
+  const [customClass, setCustomClass] = useState("");
 
-  const {
-    tasks,
-    setShowFormTask,
-    setCurrentTask,
-    setTasks,
-  } = useCustomer();
-
-  function handleEdit(id) {
+  function changeStatusTarefa() {
+    if(note.status_tarefa === "pendente") {
+      setCustomClass("container_card_title_pendente")
+    } else if(note.status_tarefa === "executando") {
+      setCustomClass("container_card_title_executando")
+    } else if(note.status_tarefa === "concluida") {
+      setCustomClass("container_card_title_concluida")
+    }
+  }
+  const getTask = (id) => {
     setShowFormTask(true);
-    const editedTask = tasks.find((task) => task.id === id);
-    setCurrentTask({
-      id: editedTask.id,
-      title: editedTask.title,
-      description: editedTask.description,
-      date: editedTask.date,
-      status: editedTask.status,
-    });
+    getTaskById(id);
   }
 
-  function handleDelete(id) {
-    const updateTask = tasks.filter((task) => task.id !== id);
-    setTasks(updateTask);
-  }
+  useEffect(() => {
+    changeStatusTarefa()
+  },[])
 
   return (
-    <div className="container_card">
+    <div className={customClass + " container_card"}>
       <div className="container_card_title">
-        <p>{title}</p>
+        <p>{note.titulo_tarefa}</p>
         <div className="container_card_buttons">
-          <button onClick={() => handleEdit(id)}>
+          <button onClick={() => getTask(note.id)}>
             <VscEdit />
           </button>
-          <button onClick={() => handleDelete(id)}>
+          <button onClick={() => deleteTaskById(note.id)}>
             <VscTrash />
           </button>
         </div>
       </div>
       <div className="container_card_texts">
         <div className="container_card_description">
-          <p>{taskDescription}</p>
+          <p>{note.descricao_tarefa}</p>
         </div>
         <div className="container_card_date">
-          <p>{taskDate}</p>
+          <p>{note.data_tarefa}</p>
         </div>
       </div>
     </div>
   );
 }
+
+export default memo(TaskCard);
