271c13d6b0b77b12dc540c93dd55900be71f76d0
Gaangb <angelob4stos@gmail.com>
2024-01-28 01:08:08 -0300

    feat: passando props

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -7,11 +7,39 @@ import Header from "../../components/Header";
 export default function Home() {
   const {
     showformTask,
+    tasks,
+    deleteTaskById,
+    getTaskById,
+    updateTask,
+    currentTask,
+    isFiltered,
+    setShowFormTask,
+    setCurrentTask,
+    setTasks,
+    addTask,
   } = useCustomer();
+
   return (
     <Container customClass={`customer_container container ${showformTask ? "modal-open" : ""}`}>
-      <Header />
-      <TasksList />
+      <Header 
+        setShowFormTask={setShowFormTask}
+        showformTask={showformTask}
+        currentTask={currentTask}
+        tasks={tasks}
+        setCurrentTask={setCurrentTask}
+        setTasks={setTasks}
+        updateTask={updateTask}
+        addTask={addTask}
+      />
+      <TasksList 
+        tasks={tasks}
+        deleteTaskById={deleteTaskById}
+        getTaskById={getTaskById}
+        isFiltered={isFiltered}
+        setShowFormTask={setShowFormTask}
+        setCurrentTask={setCurrentTask}
+        setTasks={setTasks}
+      />
     </Container>
   );
 }
