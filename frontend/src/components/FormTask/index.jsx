00e99b57c2f713431c080d72755b601ab5090796
Gaangb <angelob4stos@gmail.com>
2024-01-28 01:03:36 -0300

    feat: adicionando metodo de salvar e editar tasks

--- frontend/src/components/FormTask/index.jsx
+++ frontend/src/components/FormTask/index.jsx
@@ -1,124 +1,117 @@
-import { useParams } from "react-router-dom";
+import { format, parse } from "date-fns";
 
-import { useCustomer } from "../../hooks/CustomerHooks";
 import Button from "../Button";
 import Container from "../Container";
 import Input from "../Input";
 import "./style.css";
 
-export default function FormTask() {
-  const {
-    currentTask,
-    tasks,
-    setShowFormTask,
-    setCurrentTask,
-    setTasks,
-    setNewId,
-  } = useCustomer();
-
-  let { id } = useParams();
-
+export default function FormTask({
+  currentTask,
+  tasks,
+  setShowFormTask,
+  setCurrentTask,
+  setTasks,
+  setNewId,
+  updateTask,
+  addTask,
+}) {
   const valuesNotSet =
-    !currentTask.title ||
-    !currentTask.status ||
-    !currentTask.date ||
-    !currentTask.description;
+    !currentTask.titulo_tarefa ||
+    !currentTask.status_tarefa ||
+    !currentTask.data_tarefa ||
+    !currentTask.descricao_tarefa;
 
   function onCancelButtonClick() {
     setShowFormTask(false);
     setCurrentTask({
-      title: "",
-      status: null,
-      date: "",
-      description: "",
+      titulo_tarefa: "",
+      status_tarefa: "",
+      data_tarefa: "",
+      descricao_tarefa: "",
     });
   }
 
   function onConfirmClick() {
     if (valuesNotSet) {
       setShowFormTask(true);
-      if (!currentTask.title) {
+      if (!currentTask.titulo_tarefa) {
         alert("Digite um títule");
-      } else if (!currentTask.status) {
+      } else if (!currentTask.status_tarefa) {
         alert("Escolha um status");
-      } else if (!currentTask.date) {
+      } else if (!currentTask.data_tarefa) {
         alert("Selecione uma data");
-      } else if (!currentTask.description) {
+      } else if (!currentTask.descricao_tarefa) {
         alert("Insira uma descrição");
       }
     } else {
       if (currentTask.id) {
+        updateTask(currentTask);
         const updatedTasks = tasks.map((task) =>
           task.id === currentTask.id ? { ...currentTask } : task
         );
         setTasks(updatedTasks);
         setShowFormTask(false);
       } else {
-        const newTaskId = Date.now();
-
+        const dateObject = parse(currentTask.data_tarefa, 'yyyy-MM-dd', new Date());
+        const formattedDate = format(dateObject, 'dd-MM-yyyy');
+        const updatedTask = {
+          ...currentTask,
+          data_tarefa: formattedDate
+        };
+        addTask(updatedTask);
         setShowFormTask(false);
-        setTasks([...tasks, { ...currentTask, id: newTaskId }]);
-        setNewId(newTaskId + 1);
       }
-
       setCurrentTask({
-        title: "",
-        date: "",
-        description: "",
-        status: ""
+        titulo_tarefa: "",
+        data_tarefa: "",
+        descricao_tarefa: "",
+        status_tarefa: "",
       });
     }
   }
 
-  function handleInputChange(event) {
-    const { name, value, type } = event.target;
-
-    if (type === "radio") {
-      setCurrentTask({
-        ...currentTask,
-        [name]: value,
-      });
-    } else {
-      setCurrentTask({
-        ...currentTask,
-        [name]: value,
-      });
-    }
+  const handleInputChangeItem = (event) => {
+    console.log('event', event.target.value)
+    const { name, value } = event.target;
+    setCurrentTask({
+      ...currentTask,
+      [name]: value,
+    });
   }
 
   return (
     <Container customClass="form_container">
       <div className="form_search">
         <Input
-          name="title"
+          name="titulo_tarefa"
           type="text"
           maxLength={15}
           customClass="search form_input"
           placeholder="Adicione um título"
-          value={currentTask.title}
-          onChange={handleInputChange}
+          value={currentTask.titulo_tarefa}
+          onChange={handleInputChangeItem}
         />
       </div>
       <div className="form_search">
         <Input
-          name="date"
+          name="data_tarefa"
           type="date"
           maxLength={15}
           customClass="search form_input"
           placeholder="Digite quando aconteceu"
-          value={currentTask.date}
-          onChange={handleInputChange}
+          value={currentTask.data_tarefa}
+          onChange={handleInputChangeItem}
         />
       </div>
       <div className="form_search">
         <Input
-          name="description"
+          name="descricao_tarefa"
           type="text"
           maxLength={100}
           customClass="search form_input"
           placeholder="Descrição"
-          value={currentTask.description}
-          onChange={handleInputChange}
+          value={currentTask.descricao_tarefa}
+          onChange={handleInputChangeItem}
         />
       </div>
       <div className="radio_group">
@@ -126,32 +119,32 @@ export default function FormTask() {
         <div>
           <input
             type="radio"
-            name="status"
+            name="status_tarefa"
             value="pendente"
-            checked={currentTask.status === "pendente"}
-            onChange={handleInputChange}
+            checked={currentTask.status_tarefa.toLowerCase() === "pendente"}
+            onChange={handleInputChangeItem}
           />
           <label htmlFor="pendente">Pendente</label>
         </div>
         <div>
           <input
             type="radio"
-            name="status"
+            name="status_tarefa"
             value="executando"
-            checked={currentTask.status === "executando"}
-            onChange={handleInputChange}
+            checked={currentTask.status_tarefa.toLowerCase() === "executando"}
+            onChange={handleInputChangeItem}
           />
           <label htmlFor="executando">Executando</label>
         </div>
         <div>
           <input
             type="radio"
-            name="status"
-            value="concluída"
-            checked={currentTask.status === "concluída"}
-            onChange={handleInputChange}
+            name="status_tarefa"
+            value="concluida"
+            checked={currentTask.status_tarefa.toLowerCase() === "concluida"}
+            onChange={handleInputChangeItem}
           />
-          <label htmlFor="concluída">Concluída</label>
+          <label htmlFor="concluida">Concluída</label>
         </div>
       </div>
       <div className="form_container_button">
