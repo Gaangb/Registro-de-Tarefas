f4f2393a5e68182fecae8b1547cf4e66c4490e47
Gaangb <angelob4stos@gmail.com>
2024-01-28 01:10:21 -0300

    fix: removendo routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,14 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
-import { CustomerProvider } from './hooks/CustomerHooks';
-import Home from './pages/Home';
-import './App.css';
+import { CustomerProvider } from "./hooks/CustomerHooks";
+import Home from "./pages/Home";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-        <div className='container_geral'>
-          <CustomerProvider>
-            <Routes>
-              <Route path="/" element={<Home/>}/>
-            </Routes>
-          </CustomerProvider>
-        </div>
-    </Router>
+    <div className="container_geral">
+      <CustomerProvider>
+        <Home />
+      </CustomerProvider>
+    </div>
   );
 }
 
